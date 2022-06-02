cd75f71752b552d110f2ed5a5bb84b6a86824308
Afdi Fauzul Bahar <104412066+afdifauzul@users.noreply.github.com>
2022-06-02 20:35:01 +0700

    Update DetailMeal.js

--- pages/components/DetailMeal.js
+++ pages/components/DetailMeal.js
@@ -9,13 +9,20 @@ function DetailMeal({data}) {
     console.log(data);
     return (
         <div>
-            <h1 className={styles.title}>
-                {data.strMeal}
-            </h1>
-            <Image src={data.strMealThumb} alt="" width={250} height={250}/>
-            <div>
-                <p>{data.strInstructions}</p>
-            </div>
+            {
+                data.length > 0 ? (
+                    <><h1 className={styles.title}>
+                        {data.strMeal}
+                    </h1>
+                        <Image src={data.strMealThumb} alt="" width={250} height={250} />
+                    <div>
+                        <p>{data.strInstructions}</p>
+                    </div></>
+                ):(
+                    <p>Loading....</p>
+                )
+            }
+            
         </div>
         
     )   
