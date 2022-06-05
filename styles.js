f45425258b241daee087964c08fff8121b5f1111
Afdi Fauzul Bahar <104412066+afdifauzul@users.noreply.github.com>
2022-06-05 19:51:51 +0700

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const UseStyles = makeStyles((theme) => ({
+    paper: {
+        
+        padding: theme.spacing(2),
+        margin: '10px',
+        maxWidth: 250,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        '& .img': {
+            margin: 'auto',
+            display: 'block',
+            
+        },
+    },
+    product:{
+        flexGrow:1,
+        padding: theme.spacing(2),
+        margin: '10px',
+        width : 350,
+        maxHeight:250,
+        [theme.breakpoints.up('xs')]: {
+            background: 'grey',
+            color: 'black'
+        }
+    },
+}));
+
+export default UseStyles;
